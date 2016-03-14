b424cf078f9941c65437f14925772d1d83e3a154
Christopher Downard <christopherdownard@gmail.com>
2016-03-13 23:50:31 -0600

    Update package.json to allow devs to complete postinstall script. Split components out into dedicated files. Convert components that were able into pure renders.

---
+++ app/scripts/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+let Sidebar = (props) => {
+  render() {
+    return(
+        <div id="sidebar-wrapper">
+          <ul className="sidebar-nav">
+            <li className="sidebar-brand"><a href="#"></a></li>
+            <li><a target='_blank' href="https://github.com/alanbsmith/react-retro-sockets">View the Repo</a></li>
+            <li><a target='_blank' href="https://twitter.com/_alanbsmith">On Twitter</a></li>
+          </ul>
+        </div>
+    )
+  }
+};
+
+export default Sidebar
